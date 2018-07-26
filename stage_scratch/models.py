026120db18b743797ba0938121238a63e7f27699
Loan <loan181@hotmail.fr>
2018-07-26 13:09:48 +0200

    Models update

--- stage_scratch/models.py
+++ stage_scratch/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ConceptDescriptions(models.Model):
+    conceptDescription = models.TextField()
+    conceptBlocks = models.ImageField()
+
+
+class Concept(models.Model):
+    conceptName = models.CharField(max_length=100)
+    conceptDescription = models.OneToOneField(ConceptDescriptions, on_delete=models.SET_NULL, primary_key=True)
+
+
+class ConceptExample(models.Model):
+    exampleName = models.CharField(max_length=100)
+    exampleConcept = models.ForeignKey(Concept, on_delete=models.SET_NULL)
+    exampleText = models.TextField()
+
+
