31b1ee87edca3a8067adaf52f3f13ba0b3b68e3c
Loan <loan181@hotmail.fr>
2018-07-26 13:30:21 +0200

    Admin and models update

--- stage_scratch/admin.py
+++ stage_scratch/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Concept)
+admin.site.register(ConceptDescriptions)
+admin.site.register(ConceptExample)
+
 
 # Register your models here.
--- stage_scratch/models.py
+++ stage_scratch/models.py
@@ -5,15 +5,24 @@ class ConceptDescriptions(models.Model):
     conceptDescription = models.TextField()
     conceptBlocks = models.ImageField()
 
+    def __str__(self):
+        return self.conceptDescription
+
 
 class Concept(models.Model):
     conceptName = models.CharField(max_length=100)
     conceptDescription = models.OneToOneField(ConceptDescriptions, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.conceptName
 
 
 class ConceptExample(models.Model):
     exampleName = models.CharField(max_length=100)
     exampleConcept = models.ForeignKey(Concept, null=True, on_delete=models.SET_NULL)
     exampleText = models.TextField()
+
+    def __str__(self):
+        return self.exampleName + ' (' + str(self.exampleConcept) + ')'
 
 
