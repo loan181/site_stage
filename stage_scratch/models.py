9fa702d0f297a53dbce275af282c420755227ef3
Loan <loan181@hotmail.fr>
2018-07-26 13:16:42 +0200

    Models update 2

--- stage_scratch/models.py
+++ stage_scratch/models.py
@@ -8,12 +8,12 @@ class ConceptDescriptions(models.Model):
 
 class Concept(models.Model):
     conceptName = models.CharField(max_length=100)
-    conceptDescription = models.OneToOneField(ConceptDescriptions, on_delete=models.SET_NULL, primary_key=True)
+    conceptDescription = models.OneToOneField(ConceptDescriptions, null=True, on_delete=models.SET_NULL)
 
 
 class ConceptExample(models.Model):
     exampleName = models.CharField(max_length=100)
-    exampleConcept = models.ForeignKey(Concept, on_delete=models.SET_NULL)
+    exampleConcept = models.ForeignKey(Concept, null=True, on_delete=models.SET_NULL)
     exampleText = models.TextField()
 
 
