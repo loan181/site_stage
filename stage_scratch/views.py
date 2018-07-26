1063bf56fcd3caf1d76a2b1f3951d02cfdcfd39f
Loan <loan181@hotmail.fr>
2018-07-26 15:20:01 +0200

    Add concept simple template and url

--- stage_scratch/views.py
+++ stage_scratch/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import *
+
 
 def index(request):
     context = {
         'concepts' : Concept.objects.all()
     }
     return render(request, 'index.html', context)
+
+
+def concept(request, concept_name):
+    conceptInfo = get_object_or_404(Concept, conceptName=concept_name)
+
+    context = {
+        'concept' : conceptInfo
+    }
+    return render(request, 'concept.html', context)
 
 # Create your views here.
