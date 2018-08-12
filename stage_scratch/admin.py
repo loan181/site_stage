a18603c29cdfc53f75326ebaf07f5ef4ccd98823
Loan <loan181@hotmail.fr>
2018-08-12 21:47:18 +0200

    Better database admin layout

--- stage_scratch/admin.py
+++ stage_scratch/admin.py
@@ -2,7 +2,44 @@ from django.contrib import admin
 
 from .models import *
 
-toRegister = (Concept, ConceptExample, ConceptExercise, ConceptExerciseHint, ScratchBlock)
 
-for toReg in toRegister :
-    admin.site.register(toReg)
+class ConceptAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'conceptName', 'conceptNumber')
+    search_fields = ('conceptName',)
+
+
+class ConceptExampleAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'exampleName', 'exampleConcept', 'exampleNumber', 'exampleText')
+    search_fields = ('exampleName', 'exampleText')
+
+
+class ConceptExerciseAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'exerciseName', 'exerciseConcept', 'exerciseNumber', 'exerciseStatement')
+    search_fields = ('exerciseName', 'exerciseStatement')
+
+
+class ConceptExerciseHintAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'hintExercise', 'hintNumber', 'hintContent')
+    list_filter = ('hintExercise__exerciseConcept', 'hintExercise__exerciseNumber')
+    search_fields = ('hintContent',)
+
+
+class ScratchBlockAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'blockJson', 'blockDescription', 'relatedConcept',)
+    list_filter = ('additionalBlocks',)
+    search_fields = ('blockJson', 'blockDescription')
+
+
+toRegister = {
+    Concept : ConceptAdmin,
+    ConceptExample : ConceptExampleAdmin,
+    ConceptExercise : ConceptExerciseAdmin,
+    ConceptExerciseHint : ConceptExerciseHintAdmin,
+    ScratchBlock : ScratchBlockAdmin,
+}
+for toReg, toRegAdmin in toRegister.items() :
+    print(toReg, toRegAdmin)
+    admin.site.register(toReg, toRegAdmin)
+
+
+
